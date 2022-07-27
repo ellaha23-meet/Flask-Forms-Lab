eb456d6f2bb47a12b6befdcfead7c2e87cd1439c
ellaha23-meet <109908161+ellaha23-meet@users.noreply.github.com>
2022-07-27 17:41:58 +0300

    Lab is done.
    was fun

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,14 +8,37 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
-password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+username = "ellahazan3108"
+password = "ellahazan"
+facebook_friends=["Lia","Tal","Roni", "Maya", "Abigail", "Yasmena", "Shay", "Lotem"]
+
+for friend in facebook_friends:
+  print(friend)
 
 
-@app.route('/')  # '/' for the default page
-def login():
-  return render_template('login.html')
+
+
+@app.route('/',methods = ['GET','POST'])  # '/' for the default page
+def login(): 
+    error = None
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        return redirect(url_for('home'))    
+
+  
+@app.route('/home')  # '/' for the default page
+def home():
+     return render_template('home.html', facebook_friends=facebook_friends)
+
+@app.route('/friend_exists/<string:myfriend>',methods = ['GET','POST']) 
+def friend_exists(myfriend):
+    return "welcome to profile page %s" % myfriend 
+    return render_template('friend_exists.html', friend=myfriend, facebook_friends=facebook_friends)    
+# '/' for the default page
+
   
 
 
